8a47c0c5529f13a3159e9f225ffb819d6add2b9d
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-11-24 16:22:07 +0500

    models

--- PagesApp/admin.py
+++ PagesApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import POST
+
+admin.site.register(POST)
+
 # Register your models here.
--- PagesApp/models.py
+++ PagesApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
+class POST(models.Model) :
+    title = models.CharField(max_length=200);
+    text = models.TextField();
+    
+    def __str__(self) :
+        return self.title
+
 # Create your models here.
