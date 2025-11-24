e49b5b6e42378a18b61e8308e38646fd4d54115e
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-11-24 16:52:03 +0500

    mvt

--- PagesApp/views.py
+++ PagesApp/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from .models import POST
 
-# Create your views here.
-
-class HomePageView(TemplateView) :
+class HomePageView(ListView) :
+    model = POST;
     template_name = "home.html"
-    
-class  AboutPageView(TemplateView) :
-    template_name = "about.html"
