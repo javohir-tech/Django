3ab9d023f274f71727e275addf2d8b5bf8c4f024
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-11-24 20:36:29 +0500

    restart

--- PagesApp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import POST
-
-admin.site.register(POST)
-
-# Register your models here.
--- PagesApp/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import ListView
-from .models import POST
-
-class HomePageView(ListView) :
-    model = POST;
-    template_name = "home.html"
