5b4911937b262fce44672a2dc8b6135f6c50b938
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-11-24 13:25:37 +0500

    update

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'homepage',  # Assuming 'homepage' is an app in your project
 ]
 
 MIDDLEWARE = [
