da51b9bb49bdbedab4241139513a7f810f4aced0
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-11-25 21:57:57 +0500

    update

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,10 @@ from .models import BlogPost
 class BlogPostView(ListView) :
     model = BlogPost ;
     template_name = 'blog.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['posts'] = context['object_list']
+        return context
     
 class BlogDetailView(DetailView) :
     model = BlogPost;
