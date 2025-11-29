1d429ed5f97e3f0ce325c3308e1a69b9c5508f92
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-11-29 22:56:17 +0500

    update

--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,6 @@ from django.urls import reverse_lazy
 class SignUpView(generic.CreateView) :
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'registration/login.html'
+    template_name = 'registration/signup.html'
 
 # Create your views here.
