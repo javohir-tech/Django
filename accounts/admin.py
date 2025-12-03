d934aff1ce91b359498c40dd939d23a9cd011e61
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-12-03 15:36:04 +0500

    update

---
+++ accounts/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ["email", "username", "first_name", "last_name", "age", "is_staff"]
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("age",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("age",)}),)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
